3cca70c2b126b092030ba5b7c73afddeae08a6af
Andy Beer <AndrewBeer@RocketMortgage.com>
2021-11-01 16:56:08 -0400

    First Commit

---
+++ PigLatin/Program.cs
@@ -0,0 +1,114 @@
+﻿using System;
+using System.Linq;
+
+namespace PigLatinTest
+{
+    class PigLatin
+    {//Commented out the main method to avoid the "More than one entry point" error
+        //Changed this to an object class, removed static from the methods below to be able to test
+        //static void Main(string[] args)
+        //{
+        //    string userInput = GetInput("Please input a word or sentence to translate to pig Latin");
+        //    *Need a method to substring anything with spaces (last unit test)
+        //           - Substring separated by ' ' char, and foreach substring, run through the ToPigLatin() method
+        //           - Then combine separate 'words' into a string and return that string.
+
+        //    string translation = ToPigLatin(userInput);
+        //    Console.WriteLine(translation);
+        //}
+
+        public string GetInput(string prompt)
+        {
+            Console.WriteLine(prompt);
+            string input = Console.ReadLine().ToLower().Trim();
+            return input;
+        }
+
+        public bool IsVowel(char c)
+        {
+            char[] vowels = { 'a', 'e', 'i', 'o', 'u' };
+            
+            return c.ToString() == vowels.ToString();
+            //This method always returns false, since we are comparing a character to an array.
+            //A foreach loop with a break would work, to make sure we return a bool of True as soon as we find the first vowel.
+            //Like this:
+            //foreach (var v in vowels)
+            //{
+            //    if (c.ToString() == v.ToString())
+            //    {
+            //        isVowel = true;
+            //        break;
+            //    }
+            //    else
+            //    {
+            //        isVowel = false;
+            //    }
+            //}
+            //return isVowel;
+        }
+
+        public string ToPigLatin(string word)
+        {
+            char[] specialChars = { '@', '.', '-', '$', '^', '&' };
+            word = word.ToLower();
+            foreach(char c in specialChars)
+            {
+                foreach(char w in word)
+                {
+                    if (w == c)
+                    {
+                        Console.WriteLine("That word has special characters, we will return it as is"); 
+                        //this is working as expected, based on email format ('@' and '.')
+                        return word;
+                    }
+                }
+                
+            }
+
+            bool noVowels = true;
+            foreach(char letter in word)
+            {
+                if (IsVowel(letter))
+                {
+                    noVowels = false; 
+                    //break; keyword needed here, since we are just lookingi for the first instance of this being false.
+                }
+            }
+
+            if (noVowels)  
+            {
+                return word; 
+            }
+
+            char firstLetter = word[0];
+            string output = "placeholder";
+            if (IsVowel(firstLetter) == true)
+            {
+                output = word + "ay"; //based on spec, this should be "way", not "ay".  
+                //That change will only impact words that start with a vowel.
+            }
+            else
+            {
+                int vowelIndex = -1;
+                //Handle going through all the consonants
+                for (int i = 0; i <= word.Length; i++)
+                {
+                    if (IsVowel(word[i]) == true)
+                    {
+                        vowelIndex = i;
+                        break;
+                    }
+                }
+
+                string sub = word.Substring(vowelIndex + 1);
+                //wrong index being used above.  Should be Substring(vowelIndex); to include the first vowel.
+                string postFix = word.Substring(0, vowelIndex -1);
+                //wrong index being used above.  Should be Substring(0, vowelIndex); to exclude the first vowel.
+
+                output = sub + postFix + "ay";
+            }
+
+            return output;
+        }
+    }
+}
---
+++ PigLatin/TestProgram.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+
+namespace PigLatinTest
+{
+    public class TestProgram
+    {//added the xunit unit test suite
+        //commented main method out of Program.cs
+        //made the test class public
+        [Fact]
+        public void TestToPigLatin()
+        {
+
+            //Arrange
+            PigLatin p = new PigLatin();
+            string expected = "appleway";
+            //Act
+            string actual = p.ToPigLatin("apple");
+            //Assert
+            Assert.Equal(expected, actual);
+        }
+        [Theory]
+        [InlineData("apple", "appleway")]
+        [InlineData("heck", "eckhhay")]
+        [InlineData("strong", "ongstray")]
+        [InlineData("tommy@email.com", "tommy@email.com")]
+        [InlineData("aardvark", "aardvarkway")]
+        [InlineData("Tommy", "ommytay")]
+        [InlineData("gym", "gym")]
+        [InlineData("apple joy gym tommy@email.com strong", "appleway oyjay gym tommy@email.com ongstray")]
+        //First run of these shows that ONLY "gym" and email formatting working as expected
+
+
+        public void TestPigLatinTheory(string word, string expected)
+        {
+            PigLatin p = new PigLatin();
+            string actual = p.ToPigLatin(word);
+            Assert.Equal(expected, actual);
+        }
+        //The string with spaces breaks our Theory unit test
+    }
+    
+}
